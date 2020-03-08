bdd745e4195bbb21867efaafc15ab8bee66b1117
François Voron <fvoron@gmail.com>
2020-03-08 09:41:07 +0100

    Apply formatting

--- prerender_python_starlette/__init__.py
+++ prerender_python_starlette/__init__.py
@@ -4,7 +4,7 @@ __version__ = "0.0.0"
 
 
 from prerender_python_starlette.middleware import (  # noqa: F401
-    PrerenderMiddleware,
     DEFAULT_CRAWLER_USER_AGENTS,
     DEFAULT_EXTENSIONS_TO_IGNORE,
+    PrerenderMiddleware,
 )
